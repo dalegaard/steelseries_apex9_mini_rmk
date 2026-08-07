0247d11d2315dda8194d7c6d578305955884d250
Lasse Dalegaard <dalegaard@gmail.com>
2026-08-07 02:11:40 +0200

    Keymap fix

--- src/keymap.rs
+++ src/keymap.rs
@@ -13,7 +13,7 @@ pub const fn get_default_keymap() -> [[[KeyAction; COL]; ROW]; NUM_LAYER] {
         layer!([
             [k!(Escape), k!(Kc1), k!(Kc2), k!(Kc3), k!(Kc4), k!(Kc5), k!(Kc6), k!(Kc7), k!(Kc8), k!(Kc9), k!(Kc0), k!(Minus), k!(Equal), TRPT],
             [k!(Tab), k!(Q), k!(W), k!(E), k!(R), k!(T), k!(Y), k!(U), k!(I), k!(O), k!(P), k!(LeftBracket), k!(RightBracket), TRPT],
-            [k!(CapsLock), k!(A), k!(S), k!(D), k!(F), k!(G), k!(H), k!(J), k!(K), k!(L), k!(ScrollLock), k!(Quote), k!(NonusHash), k!(Enter)],
+            [k!(CapsLock), k!(A), k!(S), k!(D), k!(F), k!(G), k!(H), k!(J), k!(K), k!(L), k!(Semicolon), k!(Quote), k!(NonusHash), k!(Enter)],
             [k!(LShift), k!(NonusBackslash), k!(Z), k!(X), k!(C), k!(V), k!(B), k!(N), k!(M), k!(Comma), k!(Dot), k!(Slash), TRPT, k!(RShift)],
             [k!(LCtrl), k!(LGui), k!(LAlt), TRPT, k!(Space), TRPT, TRPT, k!(RAlt), mo!(1), k!(RGui), k!(RCtrl), k!(Backspace), TRPT, TRPT]
         ]),
